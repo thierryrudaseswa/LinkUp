d6606362beac9a263bd245c04159020806eb022b
thierryrudaseswa <trudaseswa@gmail.com>
2024-09-03 11:54:15 +0200

    The folder structure of the linkup

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const TabLayout = () => {
+  return (
+    <View>
+      <Text>TabLayout</Text>
+    </View>
+  )
+}
+
+export default TabLayout
+
+const styles = StyleSheet.create({})
