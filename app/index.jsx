8ea91be9e1490ebc2799ae066a3ab0abd033fc39
thierryrudaseswa <trudaseswa@gmail.com>
2024-09-03 11:25:47 +0200

    The setup of the environment of linkUp app in react native using expo

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text,View } from 'react-native'
+
+const index = () => {
+  return (
+    <View className= "flex-1 items-center justify-center"> 
+      <Text className="text-3xl">Thierry is coming</Text>
+    </View>
+  )
+}
+
+export default index;
